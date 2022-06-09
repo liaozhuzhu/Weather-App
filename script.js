e11dc67b5b225dd5800ea51b3e9bf76e768b9a76
Liao Zhu <liao.zhu.cs@gmail.com>
2022-06-08 22:25:56 -0500

    finished now need to work on styling

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ let city = document.querySelector("#city-display");
 let  description = document.querySelector("#description");
 let temperature = document.querySelector("#temp");
 
-apiKey = "2e28d7e9243bc054f306ec962e33fb4e";
+let apiKey = "2e28d7e9243bc054f306ec962e33fb4e";
 
 // Convert Kelvin to Celsius
 function convertKeltoC(val) {
-    return (val - 273).toFixed(2);
+    return (val - 271.5).toFixed(2);
 }
 
 // Fetch Method
 submitBtn.addEventListener("click", function() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + inputVal.value + "&appid" + apiKey)
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + inputVal.value + "&appid=" + apiKey)
     .then(res => res.json())
     .then(data => {
         let nameVal = data["name"];
         let desc = data["weather"][0]["description"];
         let temp = data["main"]["temp"];
-        city.innerHTML = `Weather of <span>${nameVal}</span>`
+        city.innerHTML = `<span>${nameVal}</span>'s Weather`;
+        description.innerHTML = `<span>${desc}</span>`;
+        temperature.innerHTML = `<span>${convertKeltoC(temp)}°C`;
     })
+    .catch(err => alert ("Enter a Valid City"));
 });
