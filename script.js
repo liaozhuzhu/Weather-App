7ecbd372e311169bf1f9918742d7fccf68976e73
Liao Zhu <liao.zhu.cs@gmail.com>
2022-06-08 23:51:45 -0500

    working on change unit button

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let inputVal = document.querySelector("#city-input");
 let submitBtn = document.querySelector("#submit-button");
 let city = document.querySelector("#city-display");
 let  description = document.querySelector("#description");
-let temperature = document.querySelector("#temp");
+let temperatureC = document.querySelector("#temp");
+let temperatureF = document.querySelector("#temp");
+let unitBtn = document.getElementById("unit-button");
 
 let apiKey = "2e28d7e9243bc054f306ec962e33fb4e";
 
@@ -11,17 +13,42 @@ function convertKeltoC(val) {
     return (val - 271.5).toFixed(2);
 }
 
+function convertCtoF(val) {
+    return (val * 9/5) + 32;
+}
+
 // Fetch Method
 submitBtn.addEventListener("click", function() {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + inputVal.value + "&appid=" + apiKey)
     .then(res => res.json())
     .then(data => {
         let nameVal = data["name"];
-        let desc = data["weather"][0]["description"];
-        let temp = data["main"]["temp"];
+        let desc = (data["weather"][0]["description"]).toUpperCase();
+        let tempC = convertKeltoC(data["main"]["temp"]);
+        let tempF = convertCtoF(tempC);
+
         city.innerHTML = `<span>${nameVal}</span>'s Weather`;
         description.innerHTML = `<span>${desc}</span>`;
-        temperature.innerHTML = `<span>${convertKeltoC(temp)}°C`;
+        temperatureC.innerHTML = `<span>${tempC}°C</span>`;
+        //temperatureF.innerHTML = `<span>${tempF}°F</span>`;
     })
     .catch(err => alert ("Enter a Valid City"));
+    displayUnitButton(true);
 });
+
+// Display Unit Button
+function displayUnitButton(booIn) {
+    if(booIn == true) {
+        unitBtn.style.display = "inline";
+    }
+    else {
+        unitBtn.style.display = "none";
+    }
+}
+
+// Change Unit
+function changeUnit() {
+
+}
+
+displayUnitButton(false);
